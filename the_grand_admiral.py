fd39f4dcb1572cf63f7abb46dcaadd6aac398885
Eleni Lixourioti <contact@eleni.co>
2015-07-02 20:37:14 +0100

    A great admiral is born

---
+++ the_grand_admiral.py
@@ -0,0 +1,18 @@
+import numpy
+
+
+class Admiral():
+    def __init__(self):
+        # grid = [[y for y in range(0, 10)] for x in range(0, 10)]
+        # Initialize grid
+        pass
+
+    def fire(self):
+        coords = "AI"
+        print(coords)
+        return coords
+
+
+def command_the_navy():  # main
+    ai = Admiral()
+    ai.fire()
